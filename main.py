d5a3c4caf3cfd1a5f00bebb7c39ed17d36c3f62d
henri983 <henrinyame2001@gmail.com>
2025-06-17 13:04:09 +0200

    connexion

---
+++ main.py
@@ -0,0 +1,89 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.title("Connexion")
+root.geometry("925x500+300+200")
+root.configure(bg="#fff")
+root.resizable(False, False)
+
+def connexion():
+    username = use.get()
+    password = code_word.get()
+
+    if username == "admin" and password == "1234":
+        screen = Toplevel(root)
+        screen.title("Application")
+        screen.geometry("925x500+300+200")
+        screen.config(bg="white")
+
+        Label(screen, text="Bienvenue à vous ", bg="white", font=("Microsoft YaHei UI Light", 50)).pack(expand=True)
+
+        screen.mainloop()
+    elif username != "admin" or password != "1234":
+        messagebox.showerror("Erreur", "Nom d'utilisateur ou mot de passe incorrect")
+
+    elif password != "1234":
+        messagebox.showerror("Erreur", "Mot de passe incorrect")  
+
+    elif username != "admin":
+        messagebox.showerror("Erreur", "Nom d'utilisateur incorrect")
+
+
+
+img = PhotoImage(file="login.png")
+Label(root, image=img, bg="#fff").place(x=50, y=50)
+
+frame =  Frame(root, width=350, height=350, bg="white")
+frame.place(x=480, y=70)
+
+heading = Label(frame, text="Connexion", bg="white", font=("Microsoft YaHei UI Light", 23, "bold"))
+heading.place(x=100, y=5)
+
+
+
+#################-------------------------------------------------
+
+def on_enter(e):
+    use.delete(0, 'end')
+def on_leave(e): 
+    name= use.get()
+    if name == '':
+        use.insert(0, "Nom d'utilisateur")
+    else:
+        pass   
+use = Entry(frame, width=25, fg="black", border=2, bg="white", font=("Microsoft YaHei UI Light", 11))
+use.place(x=30, y=80)
+use.insert(0, "Nom d'utilisateur")
+use.bind("FocusIn", on_enter)
+use.bind("<FocusOut>", on_leave)
+
+Frame(frame, width=295, height=2, bg='black').place(x=25, y=107)
+
+#################-------------------------------------------------
+def on_enter(e):
+    code_word.delete(0, 'end')
+def on_leave(e): 
+    name= code_word.get()
+    if name == '':
+        code_word.insert(0, "Mode de passe")
+    
+        
+code_word = Entry(frame, width=25, fg="black", border=2, bg="white", font=("Microsoft YaHei UI Light", 11))
+code_word.place(x=30, y=150)
+code_word.insert(0, "Mode de passe")
+code_word.bind("<FocusIn>", on_enter)
+code_word.bind("<FocusOut>", on_leave)
+
+Frame(frame, width=295, height=2, bg='black').place(x=25, y=177)
+
+#################-------------------------------------------------
+Button(frame, width=39, pady=7, text="Connexion", bg="#57a1f8", fg="white",command = connexion , border=0).place(x=35, y=205)
+label = Label(frame, text="Mot de passe oublié ?", bg="white", fg="black", font=("Microsoft YaHei UI Light", 9))
+label.place(x=75, y=270)
+
+Inscription = Button(frame, width=6, text="S'inscrire",cursor="hand2", bg="white", fg="#57a1f8", border=0, font=("Microsoft YaHei UI Light", 9))
+Inscription.place(x=215, y=270)
+#################-------------------------------------------------
+
+root.mainloop() 
